7a2c7f5a08e84341aff0a3605c826b126531bbea
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-03-03 14:55:21 +0530

    Dhruv Sharma Add:multiplication table

--- CheckMultiplicationTable.java
+++ CheckMultiplicationTable.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 class CheckMultiplicationTable {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the number:");
+        Scanner sc = new Scanner(System.in); // Creating a Scanner object to take user input
+		System.out.print("Enter the number:"); 
         int number = sc.nextInt();
-		int [] table = new int[10];
+		int [] table = new int[10]; // Declaring an array to store the multiplication table
 		
+		// Loop to calculate and display the multiplication table
 		for(int i=0;i<10;i++){
 			table[i]=number * (i+1);
 			System.out.println(+number+ "*"+(i+1)+"="+table[i]);
