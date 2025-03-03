97f8e7f329dddff3dcf7e045424efa2b0276d381
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-03-03 14:42:34 +0530

    Dhruv Sharma Add:check number

--- CheckNumber.java
+++ CheckNumber.java
@@ -1,40 +1,39 @@
 import java.util.Scanner;
 
-class CheckNumbers{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int [] numbers = new int[5];
-		for(int i=0;i<numbers.length;i++){
-		System.out.print("Enter the number " + (i+1)+ " :");
-		numbers[i]=sc.nextInt();
-		}
-		for(int i=0;i<numbers.length;i++){
-		if(numbers[i]>0){
-			if(numbers[i]%2==0){
-				System.out.println("The Number "+numbers[i]+" is even");
-			}
-			else{
-				System.out.println("The Number "+numbers[i]+" is odd");
-			}
-		}
-			else if (numbers[i]==0){
-			System.out.println("The Number "+numbers[i]+" is zero");
-			}
-			else{
-				System.out.println("The Number "+numbers[i]+" is negative");
-				}
-			
-			
-		}
-		if(numbers[0]==numbers[numbers.length-1]){
-			System.out.println("First and last element is equal");
-		}
-		else if (numbers[0]>=numbers[numbers.length-1]){
-			System.out.println("First element is greater than last element");
-		}
-		else {
-			System.out.println("last element is greater than first element");
-		}
-		
-	}	
+class CheckNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        // Taking user input for the array
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter the number " + (i + 1) + " : ");
+            numbers[i] = sc.nextInt();
+        }
+
+        // Checking the properties of each number
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > 0) {  // Checking if number is positive
+                if (numbers[i] % 2 == 0) {  // Checking if even
+                    System.out.println("The Number " + numbers[i] + " is even");
+                } else {  // If not even, then it's odd
+                    System.out.println("The Number " + numbers[i] + " is odd");
+                }
+            } else if (numbers[i] == 0) {  // Checking if number is zero
+                System.out.println("The Number " + numbers[i] + " is zero");
+            } else {  // If not positive or zero, then it's negative
+                System.out.println("The Number " + numbers[i] + " is negative");
+            }
+        }
+
+        // Comparing first and last elements of the array
+        if (numbers[0] == numbers[numbers.length - 1]) {
+            System.out.println("First and last element are equal");
+        } else if (numbers[0] > numbers[numbers.length - 1]) {
+            System.out.println("First element is greater than last element");
+        } else {
+            System.out.println("Last element is greater than first element");
+        }
+        
+    }
 }
