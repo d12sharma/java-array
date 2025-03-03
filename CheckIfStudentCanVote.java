4bafc5812fb6d642c8790c36cbcc1f51549fcb26
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-03-03 14:20:15 +0530

    Dhruv Sharma add : check if student can vote

--- CheckIfStudentCanVote.java
+++ CheckIfStudentCanVote.java
@@ -1,15 +1,17 @@
-import java.util.Scanner;
+import java.util.Scanner; // Import the Scanner class to take user input
 
 class CheckIfStudentCanVote {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		int [] studentAge = new int[10];
+		int [] studentAge = new int[10]; // Declare an array to store the ages of 10 students
 		
+		// Loop to take input for the age of each student
 		for(int i=0;i<studentAge.length;i++){
 		System.out.print("Enter the age of student " + (i+1)+ " :");
 		studentAge[i]=sc.nextInt();
 		}
-		for(int j=0;j<studentAge.length;j++){
+		// Loop to check if each student is eligible to vote
+		for(int j=0;j<studentAge.length;j++){ 
 		if(studentAge[j]<=0){
 		System.out.println("Invalid Age");
 		}
